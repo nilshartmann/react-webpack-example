aa9e610a87702c28aacc1bcdad00ffbace3272ac
Nils Hartmann <nils@nilshartmann.net>
2015-06-30 19:05:12 +0200

    Enable WebDevServer-Client in webpack config instead of index.html

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,24 @@ var webpack = require('webpack');
 
 module.exports = {
 	resolve: {
+		// Make sure, Webpack finds import'ed and require'd files specified without extension
+		// so 'import Bla from './Bla' makes webpack to look for files 'Bla', 'Bla.js' and 'Bla.jsx'
 		extensions: ['', '.js', '.jsx']
 	},
 	entry:  [
+		// webpack dev server with hot reloading
+		'webpack-dev-server/client?http://localhost:8080',
 		'webpack/hot/dev-server',
+
+		// path to our 'root' module
 		path.resolve(__dirname, 'src/main.js')
 	],
 	output: {
+		// output path
 		path: path.resolve(__dirname, 'public/dist'),
 		publicPath: '/dist/',
+
+		// Name of the resulting bundle file that
 		filename: 'main.js'
 	},
 	module: {
@@ -23,7 +32,10 @@ module.exports = {
 		]
 	},
 	plugins: [
+		// Enable Hot Module Replacement
 		new webpack.HotModuleReplacementPlugin(),
+
+		// Avoid publishing files when compilation failed
 		new webpack.NoErrorsPlugin()
 	],
 	stats: {
