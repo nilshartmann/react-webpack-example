b78a5192f932c284eea777662fb0f02d8c7faf94
Nils Hartmann <nils@nilshartmann.net>
2015-07-04 17:17:58 +0200

    More Footer improvements

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import NavButton from './NavButton';
 
 // Use Image resources via require
 const Icons = {
-	github: require('./styles/gh/GitHub-Mark-64px.png'),
+	github:      require('./styles/gh/GitHub-Mark-64px.png'),
 	githubLight: require('./styles/gh/GitHub-Mark-Light-64px.png')
 };
 
@@ -14,15 +14,18 @@ export default class Footer extends React.Component {
 		super(props);
 
 		// ES7 function bind proposal (https://github.com/zenparsing/es-function-bind)
-		this.onOver = ::this.onOver;
-		this.onOut = ::this.onOut;
+		this.onOver =
+	::
+		this.onOver;
+		this.onOut =
+	::
+		this.onOut;
 	}
 
 	componentWillMount() {
 		this.state = this.state || {};
 		this.state.hovered = false;
 	}
-
 
 	componentDidMount() {
 		this.footerNode = React.findDOMNode(this.refs.footer);
@@ -37,27 +40,24 @@ export default class Footer extends React.Component {
 	}
 
 	onOver() {
-		this.setState({ hovered: true });
+		this.setState({hovered: true});
 	}
 
 	onOut() {
-		this.setState({ hovered: false });
+		this.setState({hovered: false});
 	}
 
-
 	render() {
-
-		const footerLineClassName = this.state.hovered?'Footer-FooterLine Footer-FooterLine-Selected':'Footer-FooterLine';
+		const footerLineClassName = this.state.hovered ? 'FooterInner-FooterLine FooterInner-FooterLine-Selected' : 'FooterInner-FooterLine';
 
 		return (
-			<div ref="footer" className="Footer">
-				<a href="https://github.com/nilshartmann/react-webpack-example"
-					 title="Go to project GitHub page"
-					 target="_blank">
-					<img src={this.state.hovered?Icons.github:Icons.githubLight} />
-					<div className={footerLineClassName}>A React Webpack Example</div>
-				</a>
-				<br/>
+			<div className="Footer">
+					<a ref="footer" className="FooterInner" href="https://github.com/nilshartmann/react-webpack-example"
+						 title="Go to project GitHub page"
+						 target="_blank">
+							<img src={this.state.hovered?Icons.github:Icons.githubLight}/>
+							<div className={footerLineClassName}>A React Webpack Example</div>
+					</a>
 			</div>
 		)
 	}
